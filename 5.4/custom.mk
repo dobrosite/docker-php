68dd5268c58e5a3dec39f678a1ee7c0e23bd88b7
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-03-30 02:33:55 +0300

    Исправлены сборки 5.3…7.0

--- 5.4/custom.mk
+++ 5.4/custom.mk
@@ -2,6 +2,6 @@
 ## Особые настройки для PHP 5.4
 ##
 
-PHP_EXTRA_BUILD_DEPS += libmcrypt-dev
+PHP_EXTRA_BUILD_DEPS += libmcrypt-dev libssl-dev
 PHP_EXTRA_CONFIGURE_ARGS += --with-mcrypt=shared --with-mysql=shared
 XDEBUG_VERSION := 2.4.1

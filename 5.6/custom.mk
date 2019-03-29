31fed06b43f76cd46a04f03740031fa309d3f9a9
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-03-30 01:20:38 +0300

    Версия 5.6 переведена на новый механизм сборки

---
+++ 5.6/custom.mk
@@ -0,0 +1,7 @@
+##
+## Особые настройки для PHP 5.6
+##
+
+PHP_EXTRA_BUILD_DEPS += libmcrypt-dev libssl1.0-dev
+PHP_EXTRA_CONFIGURE_ARGS += --with-mcrypt=shared --with-mysql=shared
+XDEBUG_VERSION := 2.5.5

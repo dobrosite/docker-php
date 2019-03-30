cf57520f8ac0a8b85d2d34f1c1911b30032a677d
Михаил Красильников <m.krasilnikov@yandex.ru>
2019-03-30 15:19:19 +0300

    Версия 7.3 переведена на новый механизм сборки

--- 7.3/custom.mk
+++ 7.3/custom.mk
@@ -2,6 +2,7 @@
 ## Особые настройки для PHP 7.3
 ##
 
-PHP_EXTRA_BUILD_DEPS += libsodium-dev
-PHP_EXTRA_CONFIGURE_ARGS += --with-sodium=shared
+EXTRA_DEV_DEPS += libargon2-1
+PHP_EXTRA_BUILD_DEPS += libargon2-dev libssl-dev libsodium-dev
+PHP_EXTRA_CONFIGURE_ARGS += --with-password-argon2 --with-sodium=shared
 XDEBUG_VERSION=2.7.0beta1

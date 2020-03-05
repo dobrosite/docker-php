873a43b995575da9e8796681102e77a8218fd294
Михаил Красильников <m.krasilnikov@dobro.site>
2020-03-05 09:47:14 +0300

    Переход на стабильную версию xdebug в PHP 7.3

--- 7.3/custom.mk
+++ 7.3/custom.mk
@@ -5,4 +5,3 @@
 PHP_EXTRA_DEPS += libargon2-1
 PHP_EXTRA_BUILD_DEPS += libargon2-dev libssl-dev libsodium-dev
 PHP_EXTRA_CONFIGURE_ARGS += --with-password-argon2 --with-sodium=shared
-XDEBUG_VERSION=2.7.0beta1
